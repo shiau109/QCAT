8f9685d2ce4bc75d4fd1615fd06e033754e3e66e
junyeeeeeee <junyeeeee@gmail.com>
2025-03-31 12:30:03 +0800

    Update single_pulse_FFT.py

--- src/qcat/analysis/pulse_FFT/single_pulse_FFT.py
+++ src/qcat/analysis/pulse_FFT/single_pulse_FFT.py
@@ -4,22 +4,39 @@ import matplotlib.pyplot as plt
 # 定義一個時域函數（例如：正弦波加高斯波）
 gate_time = 400 # ns
 IF_freq = 50 # MHz
+pulse_num = 10
 
 T = gate_time/1000
 t = np.linspace(0, T, gate_time, endpoint=False)  # 時間軸
 
 sfactor = 4
 sigma = T / sfactor
-# envelope = np.exp(-((t - actual_gate_time/2) ** 2) / (2 * sigma**2))
-envelope = (0.318 - 0.005 * (t-T/2)**2)*(np.cos((np.pi/T)*(t-T/2)))**2
+# Square
+# envelope = np.ones(gate_time)
+# Gaussian
+Gaussina0 = np.exp(-((t[0] - T/2) ** 2) / (2 * sigma**2))
+# envelope = np.exp(-((t - T/2) ** 2) / (2 * sigma**2)) - Gaussina0
+# Parabola cos square
+# envelope = (0.318 - 0.005 * (t-T/2)**2)*(np.cos((np.pi/T)*(t-T/2)))**2
+# Cos
+envelope = 0.5 * (1 - np.cos(2 * np.pi * t / T))
+# Sinc
 
-IF_pulse = np.sin(2 * np.pi * IF_freq * t)
-f_signal = IF_pulse * envelope # 組合波形
+IF_pulse = np.cos(2 * np.pi * IF_freq * t)
+full_envelope = []
+full_IF_pulse = []
+full_t = []
+for i in range(pulse_num):
+    full_envelope = np.append(full_envelope, envelope)
+    full_IF_pulse = np.append(full_IF_pulse, IF_pulse)
+    full_t = np.append(full_t, t+T*i)
+# IF_pulse = 1
+f_signal = full_IF_pulse * full_envelope # 組合波形
 
 
 # 傅立葉變換
 f_transform = np.fft.fft(f_signal)  # 傅立葉變換
-frequencies = np.fft.fftfreq(len(t), d=(t[1] - t[0]))  # 計算頻率
+frequencies = np.fft.fftfreq(len(t)*pulse_num, d=(t[1] - t[0]))  # 計算頻率
 
 positive_freq_indices = frequencies > 0  # 過濾條件
 positive_frequencies = frequencies[positive_freq_indices]
@@ -29,7 +46,7 @@ positive_phase = np.angle(f_transform)[positive_freq_indices]    # 相位
 # 繪製原始信號
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 2, 1)
-plt.plot(t, f_signal)
+plt.plot(full_t, f_signal)
 plt.title("Time Domain Signal")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
