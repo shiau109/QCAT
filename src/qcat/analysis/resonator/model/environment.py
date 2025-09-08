f8ad22894f6648a5653b8f1cbfa90abbdc2698e6
shiau109 <shiau109@gmail.com>
2025-09-08 19:47:36 +0800

    Clean old files

--- src/qcat/analysis/resonator/model/environment.py
+++
@@ -1,8 +0,0 @@
-class TransmissionLine():
-    """
-    Properties of Transmission Line
-    """
-    def __init__( self ):
-        self.electical_delay = None
-        self.constant_delay = None
-        self.attenuation = None
