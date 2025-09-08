89a4ebac49a4d4c964c8e3b232fcf05d66055572
shiau109 <shiau109@gmail.com>
2025-09-08 19:57:28 +0800

    clean old files

--- src/tests/read_QOC_device.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-# Replace 'your_file.yaml' with the path to your YAML file
-with open(r'C:\Users\shiau\QCAT\src\tests\quantrolOx_device_config.yaml', 'r') as file:
-    data = yaml.safe_load(file)
-
-print(data["components"])
--- src/tests/read_QOX_config.py
+++
@@ -1,8 +0,0 @@
-import tomlkit
-
-# Assuming 'config.toml' is your file
-with open(r'src/tests/quantrolOx_config.toml', 'r') as file:
-    content = file.read()
-    data = tomlkit.parse(content)
-
-print(data)
